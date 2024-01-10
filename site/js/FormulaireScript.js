498d02db80c9c2322b72383462f3925b3d466089
TwitsTeen <76262637+TwitsTeen@users.noreply.github.com>
2024-01-10 09:03:01 +0100

    Delete site directory

--- site/js/FormulaireScript.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var Checkbox = document.getElementById("Autre");
-  var textBox = document.getElementById("AutreText");
-  Checkbox.addEventListener("change", function () {
-    textBox.style.visibility = Checkbox.checked ? "visible" : "hidden";
-  });
-});
